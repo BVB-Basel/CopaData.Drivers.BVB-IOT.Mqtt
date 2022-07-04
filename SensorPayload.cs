8511ee27955c9c96503877b670aa4d7fe7030cfb
Simon Jenny <simon.jenny@me.com>
2022-07-04 11:53:29 +0200

    upload@gh

---
+++ SensorPayload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CopaData.Drivers.BVB-IOT.Mqtt
+{
+    public class SensorPayload
+    {
+        public double Value { get; set; }
+
+        public DateTime LastChangeDateTime { get; set; }
+    }
+}
